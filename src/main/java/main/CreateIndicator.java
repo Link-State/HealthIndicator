210690599c58ab5e9343529bd631a00bd6d2c5f3
링크상태 <60739875+Link-State@users.noreply.github.com>
2024-01-24 17:30:27 +0900

    Fix: 문구 색상추가

--- src/main/java/main/CreateIndicator.java
+++ src/main/java/main/CreateIndicator.java
@@ -10,6 +10,7 @@ import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 
@@ -27,36 +28,47 @@ public class CreateIndicator implements Listener {
 	
 	@EventHandler
 	public void damagedByEntity(EntityDamageByEntityEvent e) {
-		Player show = null;
-		LivingEntity target = null;
-		
-		if (e.getDamager() instanceof Player) {
-			show = (Player) e.getDamager();
-			target = (LivingEntity) e.getEntity();
-		}
-		if ((e.getEntity() instanceof Player) &&
-			(e.getDamager() instanceof LivingEntity)) {
-			show = (Player) e.getEntity();
-			target = (LivingEntity) e.getDamager();
-		}
-		
-		if (show == null || target == null) {
+		if (!(e.getDamager() instanceof Player)) {
 			return;
 		}
-		
-		String target_name = target.getName();
-		double target_maxHealth = target.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
-		double target_currentHealth = target.getHealth();
-		
-		String text = String.format("%s : %,.1f / %,.1f", target_name, target_currentHealth, target_maxHealth);
-		
+		if (!(e.getEntity() instanceof LivingEntity)) {
+			return;
+		}
+
+		Player show = (Player) e.getDamager();
+		LivingEntity target = (LivingEntity) e.getEntity();
+
+		String text = getContext(target, e.getDamage());
 		TextComponent context = new TextComponent(text);
+		
 		show.spigot().sendMessage(ChatMessageType.ACTION_BAR, context);
 	}
 	
-	@EventHandler
-	public void targetedByEntity(EntityTargetLivingEntityEvent e) {
+	private String getContext(LivingEntity target, double damage) {
+		String default_color = ChatColor.WHITE + "" + ChatColor.BOLD;
+		String name = default_color + target.getName();
 		
+		double maxHealth = target.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
+		double health = target.getHealth() - damage;
+		health = health <= 0.0 ? 0.0 : health;
+		
+		String max_str = String.format(ChatColor.GOLD + "" + ChatColor.BOLD + "%,.1f", maxHealth);
+		String cur_str = "";
+		
+		// RED -> DARK_RED -> GRAY
+		double rate = health / maxHealth;
+		if (0.2 < rate && rate <= 1) {
+			cur_str = String.format(ChatColor.RED + "" + ChatColor.BOLD + "%,.1f", health);
+		}
+		else if (0.0 < rate && rate <= 0.2) {
+			cur_str = String.format(ChatColor.DARK_RED + "" + ChatColor.BOLD + "%,.1f", health);
+		}
+		else if (rate <= 0.0) {
+			cur_str = String.format(ChatColor.GRAY + "" + ChatColor.BOLD + "%,.1f", health);
+		}
+		
+		String text = String.format("%s : %s(%s%s/%s%s)", name, default_color, cur_str, default_color, max_str, default_color);
+		
+		return text;
 	}
-	
 }
